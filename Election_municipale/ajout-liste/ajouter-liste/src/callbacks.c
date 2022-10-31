d3944b33c79589ecd68b7b920b223a52913b83bc
FirasHanini <firas.hanini@esprit.tn>
2022-10-31 13:03:48 +0100

    Projet c

---
+++ Election_municipale/ajout-liste/ajouter-liste/src/callbacks.c
@@ -0,0 +1,26 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_buttonajoutliste_clicked            (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_ajout_con1_clicked                  (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
